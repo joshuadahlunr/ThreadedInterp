1ca78b174202f4b8af892d3f30fb6faa6167c0a2
Joshua Dahl <joshuadahl@unr.edu>
2024-12-03 18:15:34 -0800

    Basic Threading Ops

---
+++ operations.parallel.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <thread>
+
+#include "opcode.hpp"
+
+// NOTE: Not a valid operation
+inline std::thread* new_thread(opcode* pc, uint64_t* registers, uint8_t* stack, uint8_t* sp) {
+	registers_and_stack env;
+	std::copy(registers, registers + MEMORY_SIZE, env.memory.data());
+
+	return new std::thread([pc, env = std::move(env)]() mutable {
+		setup_enviornment(env);
+		return pc->op(pc, env.memory.data(), env.stack_boundary, env.stack_pointer);
+	});
+}
+
+// Interpret register as signed
+inline void* fork_relative(opcode* pc, uint64_t* registers, uint8_t* stack, uint8_t* sp) {
+	auto threadPC = pc + *(int64_t*)&registers[pc->a];
+	registers[pc->out] = (uint64_t)new_thread(threadPC, registers, stack, sp);
+	NEXT();
+}
+
+// Interpret arguments as signed
+inline void* fork_relative_immediate(opcode* pc, uint64_t* registers, uint8_t* stack, uint8_t* sp) {
+	auto threadPC = pc + *(int32_t*)&pc->a;
+	registers[pc->out] = (uint64_t)new_thread(threadPC, registers, stack, sp);
+	NEXT();
+}
+
+inline void* fork_to(opcode* pc, uint64_t* registers, uint8_t* stack, uint8_t* sp) {
+	auto threadPC = (opcode*)registers[pc->a];
+	registers[pc->out] = (uint64_t)new_thread(threadPC, registers, stack, sp);
+	NEXT();
+}
+
+inline void* join_thread(opcode* pc, uint64_t* registers, uint8_t* stack, uint8_t* sp){
+	std::thread* thread = (std::thread*)registers[pc->a];
+	if(thread) {
+		if(thread->joinable()) thread->join();
+		delete thread; 
+	}
+	registers[pc->a] = 0;
+	NEXT();
+}
--- test.cpp
+++ test.cpp
@@ -3,29 +3,29 @@
 
 int main(int argc, char** argv) {
 	opcode program[] = {
-		opcode{find_label, 100}.set_immediate(label2int("thread")),
+		opcode{find_label, 200}.set_immediate(label2int("thread")),
 
 		opcode{load_immediate, 1}.set_immediate(5),
 		opcode{stack_push_immediate}.set_immediate(1),
 		opcode{stack_store_i32, 0, 0, 1},
 		opcode{load_immediate, 2}.set_immediate(6),
 		opcode{add, 3, 2, 1},
-		opcode{debug_print, 0, 3, 0},
+		opcode{debug_print, 0, 3},
 		opcode{load_immediate, 4}.set_immediate(1),
 		opcode{set_if_equal, 4, 4, 0},
 		opcode{branch_relative_immediate, 0, 4}.set_branch_immediate(5),
 		// opcode{fork_relative_immediate, 5}.set_immediate_signed(5),
-		// opcode{fork_to, 5, 100},
+		opcode{fork_to, 5, 200},
 		opcode{stack_load_i32, 3, 0, 0},
-		opcode{debug_print, 0, 3, 0},
-		// opcode{join_thread, 0, 5, 0},
+		opcode{debug_print, 0, 3},
+		opcode{join_thread, 0, 5, 0},
 		opcode{halt},
 
 		// Thread
-		// opcode{label}.set_immediate(label2int("thread")),
-		// opcode{load_immediate, 1}.set_immediate(22),
-		// opcode{debug_print, 0, 1, 0},
-		// opcode{halt},
+		opcode{label}.set_immediate(label2int("thread")),
+		opcode{load_immediate, 1}.set_immediate(22),
+		opcode{debug_print, 0, 1},
+		opcode{halt},
 	};
 
 	{
